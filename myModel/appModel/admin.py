c51add3cbac5ff432c088b6c0f8954b178ca9a89
avinrosh <avinash.roshan@gmail.com>
2018-02-10 12:08:23 +0530

    Seventh

--- myModel/appModel/admin.py
+++ myModel/appModel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from appModel.models import Topic, Webpage, Accessdata
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(Accessdata)
--- myModel/appModel/models.py
+++ myModel/appModel/models.py
@@ -8,11 +8,16 @@ class Topic(models.Model):
         return self.top_name
 
 class Webpage(models.Model):
-    name=models.ForeignKey(Topic)
-    url=models.CharField(max_length=264,unique=True)
+    topic=models.ForeignKey(Topic)
+    name=models.CharField(max_length=264, unique=True)
+    url=models.URLField(unique=True)
 
     def __str__(self):
         return self.name
 
 class Accessdata(models.Model):
+    name=models.ForeignKey(Webpage)
     date=models.DateField()
+
+    def __str__(self):
+        return str(self.date)
