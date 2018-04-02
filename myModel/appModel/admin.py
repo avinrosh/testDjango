490cab3ad655774e4e2de0b804ee36ba90d21278
avinrosh <avinash.roshan@gmail.com>
2018-04-02 22:51:49 +0530

    deleted

--- myModel/appModel/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from appModel.models import Topic, Webpage, Accessdata
-
-# Register your models here.
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(Accessdata)
--- myModel/appModel/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppmodelConfig(AppConfig):
-    name = 'appModel'
